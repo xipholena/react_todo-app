91c9a26ede22363fe76ca2e5cdfece1a370da7b2
Your Name <mogolgogol.90@gmail.com>
2021-02-17 22:19:14 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.237993ceab8c171497bfb95469a06636.js"
+  "./precache-manifest.ff280a3a7a563c2ee7966f2e8a935f73.js"
 );
 
 self.addEventListener('message', (event) => {
